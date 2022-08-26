be291324c06fa4cf172a3efa796df1f56f7dfefa
Fagbenro Oluwafikayomi Caleb <fagbenrocoa@gmail.com>
2022-08-26 08:31:21 +0100

    tic-tac-toe

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let cells = document.querySelectorAll(".grid-items")
 cells.forEach(function (and) {
     and.addEventListener('click', ()=> {
         and.innerText = 'x'
+        and.classList.add('x')
     })
 }
 )
