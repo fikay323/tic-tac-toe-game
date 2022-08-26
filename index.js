2bc0719f732ce698c340906232145adcdff2f74c
Fagbenro Oluwafikayomi Caleb <fagbenrocoa@gmail.com>
2022-08-26 09:44:12 +0100

    tic-tac-toe

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+const x_class = 'x'
+const circle_class = 'circle'
+let circleTurn;
 let cells = document.querySelectorAll(".grid-items")
 cells.forEach(function(cell) {
     cell.addEventListener('click', clicked, {once : true} )
-    function clicked (e) {
-            console.log ('clicked')
-            cell.innerText += 'x'
-            cell.classList.add('x')
-    }
 })
+function clicked (e) {
+    const cell = e.target
+    const currentClass = circleTurn ? circle_class : x_class
+    placeMark(cell, currentClass)
+    swapTurns(cell)
+}
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass)
+} 
+function swapTurns (cell) {
+    circleTurn = !circleTurn
+}
