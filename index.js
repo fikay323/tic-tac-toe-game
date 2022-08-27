617690de31eced823b46ad5845186683c8719049
Fagbenro Oluwafikayomi Caleb <fagbenrocoa@gmail.com>
2022-08-27 13:57:09 +0100

    tic-tac-toe

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ let circleTurn;
 let message = document.querySelector(".Winning-message")
 let name = document.querySelector(".message")
 let x_turn =document.querySelector('strong')
+const winningCombination = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+]
+
 let first = document.getElementById("one")
 let second = one.nextElementSibling
 let third = second.nextElementSibling
@@ -13,6 +24,7 @@ let sixth = fifth.nextElementSibling
 let seventh = sixth.nextElementSibling
 let eighth = seventh.nextElementSibling
 let ninth = eighth.nextElementSibling
+
 
 let cells = document.querySelectorAll(".grid-items")
 cells.forEach(function(cell) {
@@ -26,9 +38,11 @@ cells.forEach(function(cell) {
         }
     })
 })
+
 function clicked (e) {
     const cell = e.target
     let currentClass
+
     if (circleTurn) {
         currentClass =  circle_class
         x_turn.innerText= 'X'
@@ -38,8 +52,13 @@ function clicked (e) {
         x_turn.innerText= 'O'
     }
     placeMark(cell, currentClass)
+
+    if (checkWin(currentClass)) {
+        console.log('winner');
+        message.classList.remove('hide')
+    }
+
     swapTurns()
-    checkWin()
 }
 function placeMark(cell, currentClass) {
     cell.classList.add(currentClass)
@@ -48,5 +67,10 @@ function swapTurns () {
     circleTurn = !circleTurn
 }
 
-const boardState = Array(cells.length)
-boardState.fill(null)
+function checkWin(currentClass) {
+    return winningCombination.some(combination => {
+        return combination.every(index => {
+            return cells[index].classList.contains(currentClass)
+        })
+    })
+}
